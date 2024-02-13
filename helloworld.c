69bee74ff92ce54f94f293c830e3035427790598
shivanipandey5678 <140058874+shivanipandey5678@users.noreply.github.com>
2024-02-13 22:39:54 +0530

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<math.h>
+//help
+/* hlo
+*/
+// int main(){
+//     float age = 6.9;
+//     char love='n';
+//     int dream;
+//      printf("your age is %d\n",age);
+//      printf("your love is %c\n",love);
+//     printf("hlo\n");
+//      printf("hlo\n");
+//       printf("hlo\n");
+//        printf("hlo\n");
+//        scanf("%d",&dream);
+//        printf("dream is  %d",dream);
+//     return 0;
+// }
+// int main(){
+//     int side;
+//       printf("let sides");
+//     scanf(" %d",&side);
+//     int square=(side);
+//     int power=pow(square,2);
+//     printf("square %d",power);
+
+  
+//     return 0;
+// }
+int main(){
+    int number;
+    printf("hlooooo lets check number is divisible or not\n");
+    printf("yes prime number\n");
+    scanf(" %d",&number);
+    if(number%2==1){
+        printf("yes prime number");
+    }else{
+         printf("no it's not a  prime number");
+    }
+
+}
