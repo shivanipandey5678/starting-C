3820689a1bc4c14de2fdfeb63ccec2c578c4ce4d
shivanipandey5678 <140058874+shivanipandey5678@users.noreply.github.com>
2024-02-16 23:54:22 +0530

    Add files via upload

---
+++ function.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+void hello();
+void bye();
+
+void hello(){
+    printf("hello jii");
+   
+}
+void bye(){
+    printf("bye ji");
+   
+}
+int main(){
+ hello();
+ 
+ return 0;
+}
+ 
--- helloworld.c
+++ helloworld.c
@@ -28,15 +28,56 @@
   
 //     return 0;
 // }
+// int main(){
+//     int number;
+//     printf("hlooooo lets check number is divisible or not\n");
+//     printf("yes prime number\n");
+//     scanf(" %d",&number);
+//     if(number%2==1){
+//         printf("yes prime number");
+//     }else{
+//          printf("no it's not a  prime number");
+//     }
+
+// }
 int main(){
-    int number;
-    printf("hlooooo lets check number is divisible or not\n");
-    printf("yes prime number\n");
-    scanf(" %d",&number);
-    if(number%2==1){
-        printf("yes prime number");
-    }else{
-         printf("no it's not a  prime number");
+    // int sunday;
+    // printf("is it sunday ");
+    // scanf("%d",&sunday);
+    // int raining;
+    // printf("is it raining ");
+    // scanf("%d",&raining);
+    // int monday;
+    // printf("is it monday ");
+    // scanf("%d",&monday);
+    // if( sunday && raining){
+    //     printf("soja");
+    // }else if( monday && raining){
+    //     printf("hlo go to office");
+    // }
+    // printf("is it uppercase or lower ");
+    // char character;
+    //  scanf("%c",&character);
+    //  if(character>='A' && character<='Z' ){
+    //     printf("it is a uppercase character yes bro ");
+    //  }else if((character>='a' && character<='z' )){
+    //      printf("it is a lowercase character yes bro ");
+    //  }
+    //  int sum;
+   
+
+    // int n=5;
+    // int sumi=0;
+    // for(int i=0;i<=n;i++){
+    //     sumi+=i;
+    // }
+    // printf("sum is %d",sumi);
+   
+    for(int i=10;i>=1;i--){
+         int n =5;
+       
+        printf("table is %d\n",(n*i));
     }
 
+     return 0;
 }
